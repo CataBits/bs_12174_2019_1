f957a10a89fc777e02a37239a87a01d9ed16095c
André Luferat <51307379+CataBits@users.noreply.github.com>
2020-01-28 10:32:43 -0300

    Template_v2

---
+++ js/global.js
@@ -0,0 +1,51 @@
+$(document).ready(runApp);      // Quando o documento estiver pronto, inicia a aplicação JavaScript
+
+// Aplicação principal
+function runApp() {
+
+    $(document).on('click', '#navbtn', menuToggle);     // Monitora cliques no botão do menu
+
+    $(window).resize(menuChange);                       // Monitora redimensionamento da tela
+
+}
+
+// Mostra / oculta menu
+function menuToggle() {
+
+    if ( $('#navlinks').is(':visible') ) {      // Se o menu está visível:
+        menuHide('fast');                       // Oculta o menu
+    } else {                                    // Se não:
+        menuShow('fast');                       // Mostra o menu
+    }
+
+}
+
+// Oculta o menu
+function menuHide(vel) {
+
+    $('#navbtn').removeClass('active');         // Remove a classe 'active' do botão do menu
+    $('#navbtn i').addClass('fa-bars');         // Mostra "☰" no botão do menu
+    $('#navbtn i').removeClass('fa-times');     // Oculta "X" no botão do menu
+    $('#navlinks').slideUp(vel);                // Oculta o menu
+
+}
+
+// Mostra o menu
+function menuShow(vel) {
+
+    $('#navbtn').addClass('active');            // Adiciona a classe 'active' ao botão do menu
+    $('#navbtn i').addClass('fa-times');        // Mostra "X" no botão do menu
+    $('#navbtn i').removeClass('fa-bars');      // Oculta "☰" no botão do menu
+    $('#navlinks').slideDown(vel);              // Mostra o menu
+
+}
+
+// Controla o menu ao redimensionar a tela
+function menuChange() {
+
+    menuHide(0);                                // Oculta menu
+    if (window.innerWidth > 674) {              // Se a 'viewport' é maior que 674px:
+        menuShow(0);                            // Mostra o menu
+    }
+
+}
