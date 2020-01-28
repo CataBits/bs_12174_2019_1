6dd5e51d3b66f6c6e2f544af8b26da455234e554
André Luferat <51307379+CataBits@users.noreply.github.com>
2020-01-28 11:34:57 -0300

    Sticky menu e footer

--- js/global.js
+++ js/global.js
@@ -1,3 +1,5 @@
+var num = 94;                   // Altura do menu em relação ao topo da página
+
 $(document).ready(runApp);      // Quando o documento estiver pronto, inicia a aplicação JavaScript
 
 // Aplicação principal
@@ -6,6 +8,8 @@ function runApp() {
     $(document).on('click', '#navbtn', menuToggle);     // Monitora cliques no botão do menu
 
     $(window).resize(menuChange);                       // Monitora redimensionamento da tela
+
+    $(window).bind('scroll', rolando);                  // Monitora rolagem da tela
 
 }
 
@@ -46,6 +50,19 @@ function menuChange() {
     menuHide(0);                                // Oculta menu
     if (window.innerWidth > 674) {              // Se a 'viewport' é maior que 674px:
         menuShow(0);                            // Mostra o menu
+    }
+
+}
+
+// Controla o menu ao rolar a tela
+function rolando() {
+
+    if ($(window).scrollTop() > num) {          // Se o menu atingiu o topo da tela:
+        $('nav').addClass('fixed');             // Aplica a classe que fixa o menu
+        $('main').css('margin-top', '3rem');    // Adiciona a altura do menu ao conteúdo
+    } else {                                    // Se não:
+        $('nav').removeClass('fixed');          // Remove a classe que fixa o menu
+        $('main').css('margin-top', '0');       // Remove a altura adicional do menu no conteúdo central
     }
 
 }
