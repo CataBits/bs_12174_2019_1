dd3f8da1c73077ad1b7b9a1b622e261b698380ae
André Luferat <contatos@luferat.net>
2020-01-31 19:13:05 -0300

    Update _config.php

--- _config.php
+++ _config.php
@@ -13,4 +13,15 @@ if ( $_SERVER['SERVER_NAME'] != 'localhost' ) { // Se não estamos no XAMPP:
 // Mostra erros de conexão
 if ($conn->connect_error) 
     die("Falha na conexão com o DB: " . $conn->connect_error);
+
+// Correção para caracteres acentuados no MySQL
+// ATENÇÃO! Não se esqueça de sempre criar bases de dados no formato "utf8_general_ci"
+$conn->query('SET NAMES utf8');
+$conn->query('SET character_set_connection=utf8');
+$conn->query('SET character_set_client=utf8');
+$conn->query('SET character_set_results=utf8');
+
+// MySQL com nomes de dias da semana e meses em português
+$conn->query('SET GLOBAL lc_time_names = pt_BR');
+$conn->query('SET lc_time_names = pt_BR');
 ?>
