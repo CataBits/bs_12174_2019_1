7a8bea613403b123cc47b1ed1a12896e58c56f3d
André Luferat <contatos@luferat.net>
2020-02-03 18:12:26 -0300

    database SQL

--- database.sql
+++ database.sql
@@ -15,3 +15,45 @@ CREATE TABLE contatos (
     campo2 TEXT COMMENT 'Para uso futuro.',
     status ENUM('recebido', 'lido', 'respondido', 'apagado') DEFAULT 'recebido'
 );
+
+CREATE TABLE autor (
+    id_autor INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    thumbnail VARCHAR(255),
+    nome VARCHAR(255),
+    nometela VARCHAR(127),
+    descricao TEXT,
+    nascimento DATE,
+    campo1 TEXT,
+    campo2 TEXT,
+    campo3 TEXT,
+    status ENUM('inativo', 'ativo') DEFAULT 'ativo'
+);
+
+CREATE TABLE categoria (
+    id_categoria INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255)
+);
+
+CREATE TABLE artigo (
+    id_artigo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    thumbnail VARCHAR(255),
+    titulo VARCHAR(255),
+    resumo VARCHAR(255),
+    texto LONGTEXT,
+    autor_id INT
+    campo1 TEXT,
+    campo2 TEXT,
+    campo3 TEXT,
+    status ENUM('inativo', 'ativo') DEFAULT 'ativo',
+    FOREIGN KEY (autor_id) REFERENCES autor(id_autor)
+);
+
+CREATE TABLE art_cat (
+    id_art_cat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    artigo_id INT,
+    categoria_id INT,
+    FOREIGN KEY (artigo_id) REFERENCES artigo (id_artigo),
+    FOREIGN KEY (categoria_id) REFERENCES categoria (id_categoria)
+);
